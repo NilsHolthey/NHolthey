b5bbecf91a12be73cd2395589dddaf20f379b4b6
Nils Holthey <nils.holthey@gmx.de>
2022-06-22 22:14:20 +0200

    change vh

--- src/UI/PaigeTwo/BoxContainer.jsx
+++ src/UI/PaigeTwo/BoxContainer.jsx
@@ -9,10 +9,11 @@ export const BoxContainer = styled.section`
   grid-template-rows: 1fr 1fr;
   gap: 1rem;
 
-  @media (max-width: 550px) {
+  @media (max-width: 665px) {
     grid-template-columns: 1fr;
     grid-template-rows: 1fr 1fr 1fr 1fr;
     gap: 1rem;
+    height: 150vh;
   }
 
   /* @media (max-width: 768px) {
