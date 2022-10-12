e924ff17a3a96692eee07b992c9a9400e1588653
Nils Holthey <nils.holthey@gmx.de>
2022-10-12 14:27:25 +0200

    add hover

--- src/UI/PaigeTwo/Box.jsx
+++ src/UI/PaigeTwo/Box.jsx
@@ -9,4 +9,14 @@ export const Box = styled.div`
   position: relative;
   color: #f8f8f8;
   background-color: #272c39;
+  transition: all 300ms;
+  
+  &:hover{
+    filter: grayscale(70%);
+    cursor: pointer;
+    scale:1.1;
+    box-shadow: 0 0 2px 0 darkgrey ;
+    z-index:100;
+    
+  }
 `;
