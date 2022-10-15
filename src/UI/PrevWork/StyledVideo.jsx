8ad07b28600205754749dd913d746744118e9a06
Nils Holthey <nils.holthey@gmx.de>
2022-10-15 13:58:27 +0200

    add video

---
+++ src/UI/PrevWork/StyledVideo.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledVideo = styled.video`
+  border-radius: 8px;
+
+  max-width: 375px;
+  max-height: 667px;
+`;
--- src/components/PrevWork.jsx
+++ src/components/PrevWork.jsx
@@ -1,4 +1,5 @@
 import { Container } from '../UI/PrevWork/Container';
+import { StyledVideo } from '../UI/PrevWork/StyledVideo';
 import video from '../Video/video.mp4';
 
 export default function PrevWork() {
@@ -6,7 +7,7 @@ export default function PrevWork() {
     <Container>
       <h3> Capstonproject App DidIt</h3>
       <section>
-        <video src={video} width="375" height="667" autoPlay loop muted />
+        <StyledVideo src={video} autoPlay loop muted />
       </section>
     </Container>
   );
