1eb07badb93c686a47a1a8e19120639d8d4de954
Nils Holthey <nils.holthey@gmx.de>
2023-01-22 17:30:31 +0100

    Add basic routing structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Privacy from './privacy/privacy';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'privacy',
+    element: <Privacy />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
--- src/privacy/privacy.jsx
+++ src/privacy/privacy.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-ReactDOM.createRoot(document.getElementById('rootprivacy')).render(
-  <React.StrictMode>
-    <h1>PrivacyPolicy</h1>
-  </React.StrictMode>
-);
+export default function Privacy() {
+  return (
+    <div>
+      <h1>privacy</h1>
+    </div>
+  );
+}
