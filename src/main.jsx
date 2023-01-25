f483de2b58bc5286e0df551fa8bf44c3fb05a6fa
Nils Holthey <nils.holthey@gmx.de>
2023-01-25 11:07:03 +0100

    change import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Privacy from './privacy/Privacy';
+import Privacy from './privacy/Privacy.jsx';
 import MainPage from './components/MainPage';
 
 const router = createBrowserRouter([
